97066f9332a6022a6b99fe0caba522c2dd4ef1af
daniel <daniel.yuan@me.com>
2016-06-28 04:03:03 +0800

    FIX: MongoDB连接意外中断

--- auth/userdb/mongodb/mongodb.go
+++ auth/userdb/mongodb/mongodb.go
@@ -13,8 +13,7 @@ import (
 
 // User database backed by MongoDB database.
 type mongodb struct {
-    *mgo.Database
-    users *mgo.Collection
+    session *mgo.Session
 }
 
 func init() {
@@ -38,33 +37,44 @@ func init() {
             return nil, err
         }
 
-        db := &mongodb{Database: session.DB("")}
-        db.users = db.C("users")
+        users := session.DB("").C("users")
 
-        err = db.users.EnsureIndex(mgo.Index{
+        err = users.EnsureIndex(mgo.Index{
             Key:    []string{"name"},
             Unique: true,
         })
         if err != nil {
-            db.Close()
+            session.Close()
             return nil, err
         }
 
-        err = db.users.EnsureIndexKey("namespace")
+        err = users.EnsureIndexKey("namespace")
         if err != nil {
-            db.Close()
+            session.Close()
             return nil, err
         }
 
-        return db, nil
+        return &mongodb{session}, nil
     }
 }
 
+func (db *mongodb) acquire() *mgo.Collection {
+    session := db.session.Copy()
+    return session.DB("").C("users")
+}
+
+func (db *mongodb) release(c *mgo.Collection) {
+    c.Database.Session.Close()
+}
+
 func (db *mongodb) Create(user userdb.User) error {
+    users := db.acquire()
+    defer db.release(users)
+
     basic := user.Basic()
 
     if basic.Namespace != "" {
-        n, err := db.users.Find(bson.M{"namespace": basic.Namespace}).Count()
+        n, err := users.Find(bson.M{"namespace": basic.Namespace}).Count()
         if err != nil {
             return err
         }
@@ -73,7 +83,7 @@ func (db *mongodb) Create(user userdb.User) error {
         }
     }
 
-    err := db.users.Insert(user)
+    err := users.Insert(user)
     if mgo.IsDup(err) {
         err = userdb.DuplicateUserError(basic.Name)
     }
@@ -81,9 +91,12 @@ func (db *mongodb) Create(user userdb.User) error {
 }
 
 func (db *mongodb) SetNamespace(username, namespace string) error {
+    users := db.acquire()
+    defer db.release(users)
+
     if namespace != "" {
         var user userdb.BasicUser
-        err := db.users.Find(bson.M{"namespace": namespace}).One(&user)
+        err := users.Find(bson.M{"namespace": namespace}).One(&user)
         if err != nil && err != mgo.ErrNotFound {
             return err
         }
@@ -96,13 +109,16 @@ func (db *mongodb) SetNamespace(username, namespace string) error {
         }
     }
 
-    return db.users.Update(
+    return users.Update(
         bson.M{"name": username},
         bson.M{"$set": bson.M{"namespace": namespace}})
 }
 
 func (db *mongodb) Find(name string, result userdb.User) error {
-    err := db.users.Find(bson.M{"name": name}).One(result)
+    users := db.acquire()
+    defer db.release(users)
+
+    err := users.Find(bson.M{"name": name}).One(result)
     if err == mgo.ErrNotFound {
         err = userdb.UserNotFoundError(name)
     }
@@ -110,11 +126,14 @@ func (db *mongodb) Find(name string, result userdb.User) error {
 }
 
 func (db *mongodb) Search(filter interface{}, result interface{}) error {
+    users := db.acquire()
+    defer db.release(users)
+
     resultv := reflect.ValueOf(result)
     if resultv.Kind() == reflect.Ptr && resultv.Elem().Kind() == reflect.Slice {
-        return db.users.Find(filter).All(result)
+        return users.Find(filter).All(result)
     } else {
-        err := db.users.Find(filter).One(result)
+        err := users.Find(filter).One(result)
         if err == mgo.ErrNotFound {
             err = userdb.UserNotFoundError(fmt.Sprintf("%v", filter))
         }
@@ -123,7 +142,10 @@ func (db *mongodb) Search(filter interface{}, result interface{}) error {
 }
 
 func (db *mongodb) Remove(name string) error {
-    err := db.users.Remove(bson.M{"name": name})
+    users := db.acquire()
+    defer db.release(users)
+
+    err := users.Remove(bson.M{"name": name})
     if err == mgo.ErrNotFound {
         err = userdb.UserNotFoundError(name)
     }
@@ -131,7 +153,10 @@ func (db *mongodb) Remove(name string) error {
 }
 
 func (db *mongodb) Update(name string, fields interface{}) error {
-    err := db.users.Update(bson.M{"name": name}, bson.M{"$set": fields})
+    users := db.acquire()
+    defer db.release(users)
+
+    err := users.Update(bson.M{"name": name}, bson.M{"$set": fields})
     if err == mgo.ErrNotFound {
         err = userdb.UserNotFoundError(name)
     }
@@ -139,6 +164,6 @@ func (db *mongodb) Update(name string, fields interface{}) error {
 }
 
 func (db *mongodb) Close() error {
-    db.Session.Close()
+    db.session.Close()
     return nil
 }
