319c06d7445ad9365858a285359f8a0fa7b291a4
daniel <daniel.yuan@me.com>
2016-08-24 08:27:32 +0800

    更新README

--- README.md
+++ README.md
@@ -54,16 +54,13 @@ Cloudway: <small>The way to cloud</small>
     ```shell
     #!/bin/bash -e
 
-    DOMAIN=example.com
-    CONSOLE_URL=http://api.$DOMAIN
-
+    : ${CLOUDWAY_DOMAIN:=example.com}
+    : ${CONSOLE_URL:=http://api.$DOMAIN}
     : ${CLOUDWAY_TAG:=latest}
-
-    cd "$(dirname "$BASH_SOURCE")"
 
     # start all-in-one container
     docker run -d --name cloudway-platform --restart=always \
-               -e CLOUDWAY_DOMAIN=$DOMAIN \
+               -e CLOUDWAY_DOMAIN=$CLOUDWAY_DOMAIN \
                -e CONSOLE_URL=$CONSOLE_URL \
                -v /var/run/docker.sock:/var/run/docker.sock:ro \
                -v cloudway-data:/data \
--- hack/all-in-one.sh
+++ hack/all-in-one.sh
@@ -1,17 +1,14 @@
 #!/bin/bash -e
 
-DOMAIN=example.com
-CONSOLE_URL=http://api.$domain
-
+: ${CLOUDWAY_DOMAIN:=example.com}
+: ${CONSOLE_URL:=http://api.$CLOUDWAY_DOMAIN}
 : ${CLOUDWAY_TAG:=latest}
-
-cd "$(dirname "$BASH_SOURCE")/.."
 
 # start all-in-one container
 docker run -d --name cloudway-platform --restart=always \
-           -e CLOUDWAY_DOMAIN=$DOMAIN \
+           -e CLOUDWAY_DOMAIN=$CLOUDWAY_DOMAIN \
            -e CONSOLE_URL=$CONSOLE_URL \
            -v /var/run/docker.sock:/var/run/docker.sock:ro \
            -v cloudway-data:/data \
            -p 80:80 -p 7999:7999 -p 2200:2200 \
-           icloudway/platform:${CLOUDWAY_TAG}
+           icloudway/platform:$CLOUDWAY_TAG
